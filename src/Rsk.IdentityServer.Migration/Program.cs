451ef9b9ef3c1f045d0bc89fe40c5777b3f91907
Joe Harvey <joe.harvey@rocksolidknowledge.com>
2021-08-04 10:22:59 +0100

    remove dboptions

--- src/Rsk.IdentityServer.Migration/Program.cs
+++ src/Rsk.IdentityServer.Migration/Program.cs
@@ -3,12 +3,12 @@ using System.IO;
 using System.Threading.Tasks;
 using Duende.IdentityServer.EntityFramework.DbContexts;
 using Duende.IdentityServer.EntityFramework.Options;
+using IdentityServer3.EntityFramework;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Rsk.IdentityServer.Migration.Readers;
 using Rsk.IdentityServer.Migration.Writers;
-using Rsk.IdentityServer.Migration.Writers.Interfaces;
 
 namespace Rsk.IdentityServer.Migration
 {
@@ -39,29 +39,22 @@ namespace Rsk.IdentityServer.Migration
             var services = new ServiceCollection();
 
             var identityServer4ConnectionString = config.GetValue<string>("IdentityServer4ConnectionString");
-            //ServerVersion.AutoDetect(identityServer4ConnectionString)
+
             services.AddDbContext<ConfigurationDbContext>(db => db.UseSqlServer(identityServer4ConnectionString));
             services.AddSingleton(new ConfigurationStoreOptions());
             services.AddSingleton(new OperationalStoreOptions());
 
-            services.AddScoped<IClientReader, EntityFrameworkClientReader>();
-            services.AddScoped<IScopeReader, EntityFrameworkScopeReader>();
+            var identityServer3ConnectionString = config.GetValue<string>("IdentityServer3ConnectionString");
+            
+            services.AddScoped<IClientReader>(_=> new EntityFrameworkClientReader(new ClientConfigurationDbContext(identityServer3ConnectionString)));
+            services.AddScoped<IScopeReader>(_=> new EntityFrameworkScopeReader(new ScopeConfigurationDbContext(identityServer3ConnectionString)));
 
             services.AddScoped<IClientWriter, EntityFrameworkClientWriter>();
             services.AddScoped<IApiResourceWriter, EntityFrameworkApiResourceWriter>();
             services.AddScoped<IIdentityResourceWriter, EntityFrameworkIdentityResourceWriter>();
 
             services.AddOptions();
-
-            var identityServer3ConnectionString = config.GetValue<string>("IdentityServer3ConnectionString");
-
-            services.Configure<DbOptions>(opt =>
-                {
-                    opt.IdentityServer3ClientsConnectionString = identityServer3ConnectionString;
-                    opt.IdentityServer3ScopesConnectionString = identityServer3ConnectionString;
-                    opt.IdentityServer4ConnectionString = identityServer4ConnectionString;
-                });
-
+            
             services.AddScoped<Migrator>();
 
             return services.BuildServiceProvider();
